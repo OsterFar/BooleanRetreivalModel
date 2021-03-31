7d70fd3fa657f5120e534ed53d341c2ef62dd84f
OsterFar <farazsarwar75@gmail.com>
2021-03-31 11:39:14 +0500

    initializedProject

---
+++ ProximityQuery/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProximityqueryConfig(AppConfig):
+    name = 'ProximityQuery'
---
+++ simplexQuery/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SimplexqueryConfig(AppConfig):
+    name = 'simplexQuery'
